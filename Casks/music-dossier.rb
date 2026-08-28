cefe8316165cce8ad71818f357b7855a1fcbe19d
tilo liang <tiloliang@tilodeMac-Studio.local>
2026-08-28 11:49:45 +0800

    music-dossier 0.3.0

--- Casks/music-dossier.rb
+++ Casks/music-dossier.rb
@@ -1,6 +1,6 @@
 cask "music-dossier" do
-  version "0.2.3"
-  sha256 "a073025d7c1993c0051a46c296bd652e3f281c9842a878293a9f377235aa00c5"
+  version "0.3.0"
+  sha256 "9602573ca409ea17822004b65bd2a86ce2e0537632848ad5c2b19fab8006054a"
 
   url "https://github.com/waytosea-oss/music-dossier/releases/download/v#{version}/MusicDossier-v#{version}-arm64.zip"
   name "Music Dossier"
